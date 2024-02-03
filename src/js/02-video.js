5249f50fb861db04a7e780f2a23ea5da141bdb8c
ArkadiuszSmeder <arkadiusz.smeder@gmail.com>
2024-02-03 10:54:06 +0100

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const onTimeUpdate = throttle(() => {
+    localStorage.setItem('videoplayer-current-time', player.getCurrentTime());
+  }, 1000);
+  
+  player.on('timeupdate', onTimeUpdate);
+  
+  const storedTime = localStorage.getItem('videoplayer-current-time');
+  
+  if (storedTime) {
+    player.setCurrentTime(parseFloat(storedTime));
+  }
