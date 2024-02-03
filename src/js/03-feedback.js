9d2e293d2ad2ed0034b7ba118ca08d96d2bde610
ArkadiuszSmeder <arkadiusz.smeder@gmail.com>
2024-02-03 14:49:29 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector(".feedback-form");
+
+const localStorageKey = "feedback-form-state";
+
+form.addEventListener("input", throttle((event) => {
+    let formData = {
+        email: form.elements.email.value,
+        message: form.elements.message.value,
+    };
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+}), 500);
+
+const storedFormData = localStorage.getItem(localStorageKey);
+if (storedFormData) {
+    const parsedFormData = JSON.parse(storedFormData);
+    form.elements.email.value = parsedFormData.email ?? "";
+    form.elements.message.value = parsedFormData.message ?? "";
+};
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let formData = {
+        email: form.elements.email.value,
+        message: form.elements.message.value,
+    };
+    console.log(formData);
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+});
